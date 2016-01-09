95d177afdeaa736921d087c719594baf2792b593
fai1618 <fai1.618033@gmail.com>
2016-01-09 15:18:36 +0900

    add sh file

---
+++ getLinuxDistribution.sh
@@ -0,0 +1,20 @@
+# Get Linux distribution name
+get_os_distribution() {
+    if   [ -e /etc/debian_version ] ||
+         [ -e /etc/debian_release ]; then
+        # Check Ubuntu or Debian
+        if [ -e /etc/lsb-release ]; then
+            # Ubuntu
+            distri_name="ubuntu"
+        else
+            # Debian
+            distri_name="debian"
+        fi
+    elif [ -e /etc/fedora-release ]; then
+        # Fedra
+        distri_name="fedora"
+    else
+        # Other
+        echo "unkown distribution"
+        distri_name="unkown"
+    fi
